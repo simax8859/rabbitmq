30444e985ec00b79ece1b2ed43b364543a6d94b5
ling <ljxiang1995@outlook.com>
2019-01-04 17:05:37 +0800

    测试ack功能，并测试消费者端限流功能

---
+++ api/src/main/java/com/ling/api/limit/MyConsumer.java
@@ -0,0 +1,36 @@
+package com.ling.api.limit;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+import java.io.IOException;
+
+public class MyConsumer extends DefaultConsumer {
+
+
+	private Channel channel ;
+	
+	public MyConsumer(Channel channel) {
+		super(channel);
+		this.channel = channel;
+	}
+
+	@Override
+	public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+		System.err.println("-----------consume message----------");
+		System.err.println("consumerTag: " + consumerTag);
+		System.err.println("envelope: " + envelope);
+		System.err.println("properties: " + properties);
+		System.err.println("body: " + new String(body));
+
+		/**
+		 * 参数说明：获取处理消息的DeliveryTag，是否批量处理 false
+		 */
+		channel.basicAck(envelope.getDeliveryTag(), false);
+		
+	}
+
+
+}
